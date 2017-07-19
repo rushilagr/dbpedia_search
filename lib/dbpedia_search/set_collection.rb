448ac6fc054701ef7254178996ac65d2070bd479
rushilagr <rushil.agrawal@gmail.com>
2017-07-19 18:26:53 +0530

    Minor patch in spotlight results

--- lib/dbpedia_search/set_collection.rb
+++ lib/dbpedia_search/set_collection.rb
@@ -40,11 +40,11 @@ class DbpediaSearch::SetCollection
 
   def filter_using_top_score_multiplier threshold=0
     top_score = self.inject(0) { |top, set| set.score>top ? set.score : top }
-    @sets { |set| set.score >= (threshold * top_score) }
+    @sets.select { |set| set.score >= (threshold * top_score) }
   end
 
   def filter_using_absolute_value threshold=0
-    @sets { |set| set.score >= threshold }
+    @sets.select { |set| set.score >= threshold }
   end
 
   def print
